eb1839948547afd2f9a1656bfd155ee92b33cab3
Angelo Thys <thys.angelo@gmail.com>
2023-11-02 14:40:56 +0000

    Remove unnecessary logic

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Snowman from './muneco-de-nieve.png';
+
 
 function component() {
   const element = document.createElement('div');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  const mySnowman = document.createElement('img');
-  mySnowman.src = Snowman;
-  
-  element.appendChild(mySnowman);
 
   return element;
 }
