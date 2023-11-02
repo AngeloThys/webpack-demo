76d20a1e29bc9b9b9c2dd8d2e28ec36a9ea9953c
Angelo Thys <thys.angelo@gmail.com>
2023-11-02 15:16:22 +0000

    Configure html webpack plugin in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -9,6 +10,6 @@ module.exports = {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    template: 
   },
+  plugins: [new HtmlWebpackPlugin()],
 };
