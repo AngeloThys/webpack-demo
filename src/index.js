7508b12582e11a7e961c068c2ff1ed86aa540d98
Angelo Thys <thys.angelo@gmail.com>
2023-11-02 14:09:25 +0000

    Add image management in webpack, Add logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
-import '.style.css';
+import './style.css';
+import Snowman from './muneco-de-nieve.png';
 
 function component() {
   const element = document.createElement('div');
@@ -7,6 +8,11 @@ function component() {
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  const mySnowman = document.createElement('img');
+  mySnowman.src = Snowman;
+  
+  element.appendChild(mySnowman);
 
   return element;
 }
